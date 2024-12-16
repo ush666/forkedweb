3bd4674ea997c5bde385bc62993c4039a955a892
ush666 <ungayaushliemae@gmail.com>
2024-12-16 22:23:00 +0800

    Add files via upload

---
+++ includes/sidebar.php
@@ -0,0 +1,85 @@
+
+<div id="sidebar" class="sidebar flex-column flex-shrink-0">
+    <a href="dashboard" class="logo">
+        <img id="sidebarLogo" class="img-fluid img-logo" src="../img/logo1.png" alt="" srcset="">
+    </a>
+    <ul class="nav nav-pills flex-column mb-auto">
+        <li class="side-nav-title">Home</li>
+        <li class="nav-item" title="Dashboard">
+            <a href="dashboard" id="dashboard-link" class="nav-link">
+                <i class="bi bi-speedometer2"></i>
+                <span class="fs-6 ms-2">Dashboard</span>
+            </a>
+        </li>
+        <li class="nav-item" title="Orders">
+            <a href="#" class="nav-link">
+                <i class="bi bi-cart"></i>
+                <span class="fs-6 ms-2">Orders</span>
+            </a>
+        </li>
+        <li class="side-nav-title">Reports</li>
+        <li class="nav-item" title="Sales">
+            <a href="#" class="nav-link">
+                <i class="bi bi-graph-up"></i>
+                <span class="fs-6 ms-2">Sales</span>
+            </a>
+        </li>
+        <li class="nav-item" title="Reports">
+            <a href="#" class="nav-link">
+                <i class="bi bi-file-earmark-bar-graph"></i>
+                <span class="fs-6 ms-2">Reports</span>
+            </a>
+        </li>
+        <li class="side-nav-title">Settings</li>
+        <li class="nav-item" title="Products">
+            <a href="products" id="products-link" class="nav-link">
+                <i class="bi bi-box"></i>
+                <span class="fs-6 ms-2">Products</span>
+            </a>
+        </li>
+        <li class="nav-item" title="Accounts">
+            <a href="#" class="nav-link">
+                <i class="bi bi-people"></i>
+                <span class="fs-6 ms-2">Accounts</span>
+            </a>
+        </li>
+    </ul>
+</div>
+
+<div id="sidebar-mobile" class="sidebar flex-column flex-shrink-0">
+    <a href="dashboard" class="logo">
+        <img id="sidebarLogo" class="img-fluid img-logo" src="../img/logo.png" alt="" srcset="">
+    </a>
+    <ul class="nav nav-pills flex-column mb-auto">
+        <li class="nav-item" title="Dashboard">
+            <a href="dashboard" id="dashboard-link" class="nav-link">
+                <i class="bi bi-speedometer2"></i>
+            </a>
+        </li>
+        <li class="nav-item" title="Orders">
+            <a href="#" class="nav-link">
+                <i class="bi bi-cart"></i>
+            </a>
+        </li>
+        <li class="nav-item" title="Sales">
+            <a href="#" class="nav-link">
+                <i class="bi bi-graph-up"></i>
+            </a>
+        </li>
+        <li class="nav-item" title="Reports">
+            <a href="#" class="nav-link">
+                <i class="bi bi-file-earmark-bar-graph"></i>
+            </a>
+        </li>
+        <li class="nav-item" title="Products">
+            <a href="products" id="products-link" class="nav-link">
+                <i class="bi bi-box"></i>
+            </a>
+        </li>
+        <li class="nav-item" title="Accounts">
+            <a href="#" class="nav-link">
+                <i class="bi bi-people"></i>
+            </a>
+        </li>
+    </ul>
+</div>
